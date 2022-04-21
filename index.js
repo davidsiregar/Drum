8e4ea8db107264e2f68aeb2a3d305b0de2d82eeb
davidS <davidsiregar699@gmail.com>
2022-04-21 11:24:30 +0700

    1.0.0

---
+++ index.js
@@ -0,0 +1,68 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+   let buttonInnerHtml=this.innerHTML;
+
+   makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+  });
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+function makeSound(key){
+  switch(key){
+    case "w":
+      let tom1=  new Audio("sounds/tom-1.mp3")
+      tom1.play();
+      break;
+
+      case "a":
+       let tom2=  new Audio("sounds/tom-2.mp3")
+       tom2.play();
+       break;
+
+       case "s":
+         let tom3=  new Audio("sounds/tom-3.mp3")
+         tom3.play();
+         break;
+
+         case "d":
+       let tom4=  new Audio("sounds/tom-4.mp3")
+       tom4.play();
+       break;
+
+       case "j":
+       let tom5=  new Audio("sounds/snare.mp3")
+       tom5.play();
+       break;
+
+       case "k":
+       let tom6=  new Audio("sounds/crash.mp3")
+       tom6.play();
+       break;
+
+       case "l":
+       let tom7=  new Audio("sounds/kick-bass.mp3")
+       tom7.play();
+       break;
+
+       default: console.log(buttonInnerHtml);
+  }
+}
+
+function buttonAnimation(currentKey){
+
+  let activeButton= document.querySelector("."+ currentKey)
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+    
+  }, 50);
+}
